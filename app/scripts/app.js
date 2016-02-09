5d99a0da4e25684004a9ac180eecf4a79088bb32
Jordan Epps <jordan.k.epps@gmail.com>
2016-02-08 22:35:24 -0800

    checkpoint-3 completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,23 @@
-angular.module('blocJams', []);
+(function() {
+	function config($stateProivder, $locationProvider) {
+		$locationProvider
+			.html5Mode({
+				enabled: true,
+				requireBase: false
+		});
+		
+		$stateProivder
+			.state('landing', {
+				url: '/',
+				templateUrl: '/templates/landing.html'
+		})
+			.state('album', {
+				url: '/album',
+				templateUrl: '/templates/album.html'
+		});
+	}
+	
+	angular
+		.module('blocJams', ['ui.router'])
+		.config(config);
+})();
