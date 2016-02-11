b3cfb567eb76b6ee834ba4be22d6a44f9e54d505
Jordan Epps <jordan.k.epps@gmail.com>
2016-02-10 22:23:16 -0800

    assignment 7 completed. created playSong function for SongPlayer service and refactored

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -2,7 +2,12 @@
 	function SongPlayer() {
 		var SongPlayer = {};
 		
+		/**
+		*@desc Currently playing song
+		*@type {Object}
+		*/
 		var currentSong = null;
+		
 		/**
 		* @desc Buzz object audio file
 		* @type {Object}
@@ -28,20 +33,37 @@
 			currentSong = song;
 		};
 		
+		/**
+		* @function playSong
+		* @desc Starts playing a song and sets song.playing to true
+		* @param {Object} song
+		*/		
+		var playSong = function(song) {
+			currentBuzzObject.play();
+			song.playing = true;
+		}
+		
+		/**
+		* @function SongPlayer.play
+		* @desc Checks to see if the currentSong is equal to the song passed in. If it isn't, starts playing the new song. If it is, it pauses the currently playing song.
+		* @param {Object} song
+		*/
 		SongPlayer.play = function(song) {
 			if (currentSong !== song) {
 				setSong(song);
-				
-				currentBuzzObject.play();
-				song.playing = true;
+				playSong(song);
 			} else if (currentSong == song) {
 				if (currentBuzzObject.isPaused()) {
-					currentBuzzObject.play();
-					song.playing = true;
+					playSong(song);
 				}
 			}
 		};
 		
+		/**
+		* @function SongPlayer.pause
+		* @desc pauses the currently playing song
+		* @param {Object} song
+		*/
 		SongPlayer.pause = function(song) {
 			currentBuzzObject.pause();
 			song.playing = false;
