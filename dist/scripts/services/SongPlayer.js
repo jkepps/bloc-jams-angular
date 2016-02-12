4db0f72b2d86ad8585db7385ed8e031cf899d3fc
Jordan Epps <jordan.k.epps@gmail.com>
2016-02-12 13:55:48 -0800

    checkpoint 10 completed

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-	function SongPlayer(Fixtures) {
+	function SongPlayer($rootScope, Fixtures) {
 		var SongPlayer = {};
 		
 		/**
@@ -48,6 +48,12 @@
 				formats: ['mp3'],
 				preload: true
 			});
+
+			currentBuzzObject.bind('timeupdate', function() {
+				$rootScope.$apply(function() {
+					SongPlayer.currentTime = currentBuzzObject.getTime();
+				});
+			});
 			
 			SongPlayer.currentSong = song;
 		};
@@ -66,6 +72,12 @@
 		*@type {Object}
 		*/
 		SongPlayer.currentSong = null;
+
+		/**
+		* @desc Current playback time (in seconds) of currently playing song
+		* @type {Number}
+		*/
+		SongPlayer.currentTime = null;
 		
 		/**
 		* @function SongPlayer.play
@@ -128,11 +140,22 @@
 				playSong(song);
 			}
 		};
+
+		/**
+		* @function setCurrentTime
+		* @desc Set current time (in seconds) of currently playing song
+		* @oaram {Number} time
+		*/
+		SongPlayer.setCurrentTime = function(time) {
+			if (currentBuzzObject) {
+				currentBuzzObject.setTime(time);
+			}
+		};
 		
 		return SongPlayer;
 	}
 	
 	angular
 		.module('blocJams')
-		.factory('SongPlayer', ['Fixtures', SongPlayer]);
+		.factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
